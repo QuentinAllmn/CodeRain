cd43621c872798ad0ce638dd1f835e29cf58ada6
Quentin-Allmn <q.alleman@mtp.ecolescreatives.com>
2022-01-08 17:30:47 +0100

    Update Tableau.js

--- Tableau.js
+++ Tableau.js
@@ -1,1 +1,39 @@
 ﻿
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const hangeul = '림가각간 갈 감갑ㄲ까깨꼬꼭꽃꾀ㄶ ㄺ ㄻ ㄼ ㄽ ㄾ ㄿ ㅀ ㅄㅏ ㅑ ㅓ ㅕ ㅗ ㅛ ㅜ ㅠ ㅡ ㅣㅐ ㅒ ㅔ ㅖ ㅘ ㅝ ㅢ ㅙ ㅟ ㅚ ㅞㄱ ㄴ ㄷ ㄹ ㅁ ㅂ ㅅ ㅇ ㅈ ㅊ ㅋ ㅌ ㅍ ㅎㄲ ㄸ ㅃ ㅆ ㅉ';
+const nums = '0123456789';
+
+const alphabet = katakana + hangeul + nums;
+
+const fontSize = 16;
+const columns = canvas.width / fontSize;
+
+const rainDrops = [];
+
+for (let x = 0; x < columns; x++) {
+    rainDrops[x] = 1;
+}
+const draw = () => {
+    context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = '#0F0';
+    context.font = fontSize + 'px monospace';
+
+    for (let i = 0; i < rainDrops.length; i++) {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+            rainDrops[i] = 0;
+        }
+        rainDrops[i]++;
+    }
+};
+setInterval(draw, 30);
